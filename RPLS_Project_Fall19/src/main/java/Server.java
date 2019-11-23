045c6ec4868f27a109f72428f6346ce260989488
mjures3 <58094147+mjures3@users.noreply.github.com>
2019-11-22 18:08:33 -0600

    Update Server.java

--- RPLS_Project_Fall19/src/main/java/Server.java
+++ RPLS_Project_Fall19/src/main/java/Server.java
@@ -1,214 +1,97 @@
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.Serializable;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.ArrayList;
-import java.util.function.Consumer;
 
-//import Server.ClientThread;
-//import Server.TheServer;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
-public class Server {
+public class RPLS extends Application {
 	
-	int count = 1;
-	int portNum;
-	ArrayList<ClientThread> clients = new ArrayList<ClientThread>();
-	TheServer server;
-	GameInfo info; 
-	private Consumer<Serializable> callback;
+	Text askPort, errorPort;
+	TextField portNumber;
+	Button bt1;
+	HBox portBox;
+	VBox serverBox;
+	Scene serverStart, gameScene;
+	Server theServer;
+	BorderPane pane;
 	
-	Server(Consumer<Serializable> call, int pNum){
-	
-		callback = call;
-		portNum = pNum;
-		info = new GameInfo();
-		server = new TheServer();
-		server.start();
-		
+	ListView<String> gameInfo;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		launch(args);
 	}
-	
-	
-	public class TheServer extends Thread{
-		
-		public void run() {
-		
-			try(ServerSocket mysocket = new ServerSocket(portNum);){
-		    System.out.println("Server is waiting for a client!");
-			
-		    while(true) {
-		
-				ClientThread c = new ClientThread(mysocket.accept(), count);
-				callback.accept("client has connected to server: " + "client #" + count);
-				clients.add(c);
-				c.start();
-				
-				//info.message = "new client on server: client #"+count;
-				//updateClients("new client on server: client #"+count);
-				//updateClients(info);
-				
-				count++;
-				
-			    }
-			}//end of try
-				catch(Exception e) {
-					callback.accept("Server socket did not launch");
-				}
-			}//end of while
-		}
-	
 
-		class ClientThread extends Thread{
-			
+	//feel free to remove the starter code from this method
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		primaryStage.setTitle("RPLS!!!");
+		askPort = new Text("Input Port Number: ");
+		errorPort = new Text();
+		portNumber = new TextField();
+		bt1 = new Button("Connect");
 		
-			Socket connection;
-			int count;
-			boolean inGame;
-			ObjectInputStream in;
-			ObjectOutputStream out;
-			
-			ClientThread(Socket s, int count){
-				this.connection = s;
-				this.count = count;	
-				inGame = false;
-			}
-			
-//			public void updateClients(String data) {
-//				for(int i = 0; i < clients.size(); i++) {
-//					ClientThread t = clients.get(i);
-//					try {
-//					 t.out.writeObject(data);
-//					}
-//					catch(Exception e) {}
-//				}
-//			}
-			
-			public void updateClients(GameInfo data) {
-				synchronized(data) {
-				for(int i = 0; i < clients.size(); i++) {
-					System.out.println("Hi");
-					//ClientThread t = clients.get(i);
-					try {
-				     //out.reset();
-					 clients.get(i).out.writeObject(data);
-					 out.reset();
-					}
-					catch(Exception e) {}
+		gameInfo = new ListView<String>();
+		
+		bt1.setOnAction(e-> {
+			try {
+				int pNum = Integer.parseInt(portNumber.getText());
+				if (pNum < 0 || pNum > 65535) {
+					errorPort.setText("Error: Invalid range for port number");
+					return;
 				}
-				}
-			}
-			
-//			public void updatePlayerLobby(ArrayList<ClientThread> list) {
-//				for(int i = 0; i < clients.size(); i++) {
-//					ClientThread t = clients.get(i);
-//					try {
-//				     //out.reset();
-//					 t.out.writeObject(list);
-//					 out.reset();
-//					}
-//					catch(Exception e) {}
-//				}
-//			}
-			
-			public void evalPlay() {
-			    if(info.p1Plays.equals(info.p2Plays)) {
-			    	info.message = "Round tied";
-			    }
-				else if(info.p1Plays.equals("rock") && (info.p2Plays.equals("scissor") 
-											   ||  info.p2Plays.equals("lizard"))) {
-					info.p1Points++;
-					info.message = "Player 1 won the round";
-				}
-				else if(info.p1Plays.equals("scissor") && (info.p2Plays.equals("paper") 
-						   					   	        ||  info.p2Plays.equals("lizard"))) {
-					info.p1Points++;
-					info.message = "Player 1 won the round";
-				}
-				else if(info.p1Plays.equals("paper") && (info.p2Plays.equals("rock") 
-				   	        						 ||  info.p2Plays.equals("spock"))) {
-					info.p1Points++;
-					info.message = "Player 1 won the round";
-				}
-				else if(info.p1Plays.equals("lizard") && (info.p2Plays.equals("paper") 
-													  ||  info.p2Plays.equals("spock"))) {
-					info.p1Points++;
-					info.message = "Player 1 won the round";
-				}
-				else if(info.p1Plays.equals("spock") && (info.p2Plays.equals("rock") 
-						  							 ||  info.p2Plays.equals("scissor"))) {
-					info.p1Points++;
-					info.message = "Player 1 won the round";
-				}
-			    else {
-			    	info.p2Points++;
-			    	info.message = "Player 2 won the round";
-			    }
-			    info.p1Plays = "";
-		    	info.p2Plays = "";
-		    	updateClients(info);
-			}
-			
-			public void run(){
-					
-				try {
-					in = new ObjectInputStream(connection.getInputStream());
-					out = new ObjectOutputStream(connection.getOutputStream());
-					connection.setTcpNoDelay(true);	
-				}
-				catch(Exception e) {
-					System.out.println("Streams not open");
-				}
-				
-				//info.message = "new client on server: client #"+count;
-				//updateClients("new client on server: client #"+count);
-				info.players.add("client " + count);
-				//updatePlayerLobby(clients);
-				updateClients(info);
-					
-				 while(true) {
-					    try {
-//					    	if(clients.size() == 2) info.has2Players = true;
-//							else {
-//								info.has2Players = false;
-//								continue;
-//							}
-					    	
-					    	String data = in.readObject().toString();
-					    	//info = (GameInfo)in.readObject();
-					    	callback.accept("client: " + count + " sent: " + data);
-					    	//callback.accept(info);
-					    	updateClients(info);
-					    	
-					    	//currently hardcoded: NEEDS fixing
-					    	if(count == 1) {
-					    		info.p1Plays = data;
-					    	}
-					    	else info.p2Plays = data;
-					    	
-					    	//info.message = "client #"+count+" played: "+data;
-					    	//updateClients("client #"+count+" played: "+data);
-					    	//updateClients(info);
-					    	
-					    	//MIGHT BE THE PROBLEM
-					    	if(!info.p1Plays.isEmpty() && !info.p2Plays.isEmpty()) {
-					    		evalPlay();
-					    	}
-					    }
-					    catch(Exception e) {
-					    	callback.accept("OOOOPPs...Something wrong with the socket from client: " + count + "....closing down!");
-					    	info.message = "Client #"+count+" has left the server!";
-					    	//updateClients("Client #"+count+" has left the server!");
-					    	info.players.remove("client " + count);
-					    	clients.remove(this);
-					    	updateClients(info);
-					    	break;
-					    }
-					}
-				}//end of run
-			
-			
-		}//end of client thread
+				primaryStage.setScene(gameScene);
+				primaryStage.setTitle("Game Info");
+				theServer = new Server(data -> {
+					Platform.runLater(() -> {
+						gameInfo.getItems().add(data.toString());
+					});
+				}, pNum);
+
+			}catch (NumberFormatException error) {
+				errorPort.setText("Error: input valid number");
+			} 
+		});
+		
+		primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent t) {
+                Platform.exit();
+                System.exit(0);
+            }
+        });
+		
+		//--------------First connect scene----------------------//
+		portBox = new HBox(askPort, portNumber, errorPort);
+		portBox.setAlignment(Pos.CENTER);
+		serverBox = new VBox(200, portBox, bt1);
+		serverBox.setAlignment(Pos.CENTER);
+		serverBox.setStyle("-fx-background-color: lightgreen");
+		serverStart = new Scene(serverBox, 500, 500);
+		//-------------------------------------------------------//
+		
+		//--------------Game info scene--------------------------//
+	    pane = new BorderPane();
+		pane.setStyle("-fx-background-color: coral");
+		pane.setPadding(new Insets(70));
+		pane.setCenter(gameInfo);
+		gameScene = new Scene(pane, 500, 500);
+		//-------------------------------------------------------//
+		
+		primaryStage.setScene(serverStart);
+		primaryStage.show();
+	}
+
 }
-
-
